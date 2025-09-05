3e5d11752eb02820922fec70e6cec63f358ffcee
Kori Nikhil <154355081+korinikhil@users.noreply.github.com>
2025-09-05 20:27:58 +0530

    blog updates

--- app/page.js
+++ app/page.js
@@ -9,8 +9,6 @@
   import Testimonials from "./Customer/page";
   import Loader from "./component/Loader";
   import Link from "next/link";
-import BlogPage from "./blog/page";
-  // import BlogPage from "./blog/page";
 
   export default function Hero() {
     // Apna WhatsApp number dalna (country code ke sath, bina + ke)
@@ -107,7 +105,6 @@ import BlogPage from "./blog/page";
         </section>
         <About />
         <Smart />
-        <BlogPage/>
         <LatestProjects />
         <Testimonials />
       </>
